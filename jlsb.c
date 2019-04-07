de62d7705c3ff4284e6de15ee39f28254e4add08
JonathasBarreto <42970380+JonathasBarreto@users.noreply.github.com>
2019-04-07 11:10:51 -0300

    Update jlsb.c

--- jlsb.c
+++ jlsb.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define T 52
 
 char pos_ordem(char pre_ordem[], char ordem[]) {
     int j = 0;
@@ -13,17 +14,17 @@ char pos_ordem(char pre_ordem[], char ordem[]) {
 
 int main(void) {
     int teste, nos, i; 
-    char pre_ordem[100], ordem[100];
+    char pre_ordem[T], ordem[T];
     scanf("%d", &teste); 
     
     if(teste <= 2000){
         for (; i < teste; i++) {
             scanf("%d", &nos); 
-            if(nos>=1 && nos<=52){
+            if(nos>=1 && nos<=T){
                 scanf("%s %s", pre_ordem, ordem);
                 pos_ordem(pre_ordem, ordem);
                 printf("\n");
-            }
+            }else break;
         }
     }
 }
